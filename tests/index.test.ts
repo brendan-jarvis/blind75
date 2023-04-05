069dc6f87d371f69d79c507af62d0dd97b9ed4ea
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-05 14:30:31 +1200

    Contains Duplicate #217 tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,30 @@
-import { sayHello } from '../src/index'
+// import all functions from the index file
+import * as leetcode from '../src/index'
 
-describe('sayHello', () => {
-  it('should return a welcome message', () => {
-    expect(sayHello()).toBe('Welcome to the Blind 75 on TypeScript')
+describe('sayHello function', () => {
+  it('should return a default welcome message', () => {
+    expect(leetcode.sayHello()).toBe('Welcome to the Blind 75 on TypeScript')
+  })
+
+  it('should return a custom welcome message', () => {
+    expect(
+      leetcode.sayHello('This is not the default welcome message, hooray!')
+    ).toBe('This is not the default welcome message, hooray!')
+  })
+})
+
+describe('Contains Duplicate #217', () => {
+  it('should return true if the array contains duplicates', () => {
+    expect(leetcode.containsDuplicate([1, 2, 3, 1])).toBe(true)
+  })
+
+  it('should return false if the array does not contain duplicates', () => {
+    expect(leetcode.containsDuplicate([1, 2, 3, 4])).toBe(false)
+  })
+
+  it('should return true on longer arrays', () => {
+    expect(leetcode.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBe(
+      true
+    )
   })
 })
