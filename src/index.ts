d1cf143e90c2be819bea651df56b5916700f2bc5
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-05 14:29:58 +1200

    Contains Duplicate #75

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export const sayHello = (
-  welcome = 'Welcome to the Blind 75 on TypeScript'
-): string => welcome
+export const containsDuplicate = (nums: number[]): boolean => {
+  // Set only allows unique values
+  // If this set has the same size as the original array, there are no duplicates
+  const set = new Set(nums)
+  return set.size !== nums.length
+}
