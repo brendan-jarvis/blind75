226f873db3cce3a614c92e9b05c05dc19a7aec4d
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-05 15:00:10 +1200

    Valid Anagram #242 solution

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,18 @@ export const containsDuplicate = (nums: number[]): boolean => {
   const set = new Set(nums)
   return set.size !== nums.length
 }
+
+export const isAnagram = (s: string, t: string): boolean => {
+  // If the strings are not the same length, they cannot be anagrams
+  if (s.length !== t.length) {
+    return false
+  }
+  // Create an array each string characters in lowercase
+  // Sort the arrays
+  // Join the arrays back into strings
+  // Compare the strings
+  return (
+    s.toLowerCase().split('').sort().join('') ===
+    t.toLowerCase().split('').sort().join('')
+  )
+}
