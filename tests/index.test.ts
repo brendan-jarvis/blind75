2542808e6e04d44841788c3dda3515ec7f277e4c
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-05 14:30:56 +1200

    Remove sayHello tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,18 +1,6 @@
 // import all functions from the index file
 import * as leetcode from '../src/index'
 
-describe('sayHello function', () => {
-  it('should return a default welcome message', () => {
-    expect(leetcode.sayHello()).toBe('Welcome to the Blind 75 on TypeScript')
-  })
-
-  it('should return a custom welcome message', () => {
-    expect(
-      leetcode.sayHello('This is not the default welcome message, hooray!')
-    ).toBe('This is not the default welcome message, hooray!')
-  })
-})
-
 describe('Contains Duplicate #217', () => {
   it('should return true if the array contains duplicates', () => {
     expect(leetcode.containsDuplicate([1, 2, 3, 1])).toBe(true)
