7ceddd4c6bdf56a6502b4c155eb579d16edc840e
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-25 22:58:13 +1200

    Type Challenge 13

---
+++ src/type-challenges.ts
@@ -0,0 +1,9 @@
+// Type Challenges
+
+// 13. Hello World
+type HelloWorld = string // expected to be a string
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect, NotAny } from '@type-challenges/utils'
+
+type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>]
