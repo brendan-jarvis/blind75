40f0f920cfe5b8a29f5d18ef37c5bd7d7a8bd06f
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-05 14:38:59 +1200

    Array pointer

--- src/C/pointer.c
+++ src/C/pointer.c
@@ -25,4 +25,12 @@ int main()
 
   intPtr -= 2;
   printf("intPtr decremented value: %d\t%p\n", *intPtr, intPtr);
+
+  // Should be the same for an array
+  int intArr[] = {1, 2, 3};
+  int *intArrPtr = intArr;
+  printf("intArrPtr value: %d\n", *intArrPtr);
+
+  intArrPtr++;
+  printf("intArrPtr incremented value: %d\t%p\n", *intArrPtr, intArrPtr);
 }
