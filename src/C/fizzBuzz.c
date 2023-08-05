d353eb88db387725d20817adef296247ba3ef388
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-05 12:47:43 +1200

    FizzBuzz function

--- src/C/fizzBuzz.c
+++ src/C/fizzBuzz.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char *fizzBuzz(int number);
 
 int main()
 {
   int i;
   for (i = 1; i < 50; i++)
   {
-    if (i % 15 == 0)
-    {
-      printf("FizzBuzz\n");
-    }
-    else if (i % 3 == 0)
-    {
-      printf("Fizz\n");
-    }
-    else if (i % 5 == 0)
-    {
-      printf("Buzz\n");
-    }
-    else
-    {
-      printf("%d\n", i);
-    }
+    printf("%s\n", fizzBuzz(i));
+  }
+}
+
+char *fizzBuzz(int number)
+{
+  if (number % 15 == 0)
+  {
+    return "FizzBuzz";
+  }
+  else if (number % 3 == 0)
+  {
+    return "Fizz";
+  }
+  else if (number % 5 == 0)
+  {
+    return "Buzz";
+  }
+  else
+  {
+    char *result = malloc(4 * sizeof(char));
+    sprintf(result, "%d", number);
+    return result;
   }
 }
