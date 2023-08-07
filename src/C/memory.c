8ea974b2228899b04a4647581bd2092012fdb390
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-07 15:33:03 +1200

    Haiku struct

--- src/C/memory.c
+++ src/C/memory.c
@@ -6,4 +6,11 @@ int main()
 
   // print the string and the size of the string
   printf("%s\t%lu bytes\n", str, sizeof(str));
+
+  // haiku typedef
+  typedef struct
+  {
+    char content[17];
+    char name[5];
+  } Haiku;
 }
