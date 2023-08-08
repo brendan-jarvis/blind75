5cf07aeb33f3ebf76be25205ab80bf1fb4c29800
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-09 11:55:44 +1200

    Array length

--- src/C/arrays.c
+++ src/C/arrays.c
@@ -5,7 +5,7 @@ int main()
 {
   int coordinates[5] = {1, 2, 3, 4, 5};
 
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < sizeof(coordinates) / sizeof(coordinates[0]); i++)
   {
     printf("%d\t", coordinates[i]);
   }
