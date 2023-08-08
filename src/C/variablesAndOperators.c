09b7e4eb8bb12835b80706d7ae63d1d9677cae90
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-08 13:38:01 +1200

    Number as a char

--- src/C/variablesAndOperators.c
+++ src/C/variablesAndOperators.c
@@ -5,15 +5,18 @@ int main()
   int number;
   float decimal;
   char letter;
+  char digit;
   long int bigNumber;
 
   number = 10;
   decimal = 3.14;
   letter = 'A';
+  digit = '3';
   bigNumber = 1234567890;
 
   printf("number: %d\n", number);
   printf("decimal: %f\n", decimal);
   printf("letter: %c\n", letter);
+  printf("digit: %c\n", digit);
   printf("bigNumber: %ld\n", bigNumber);
 }
