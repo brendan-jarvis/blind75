fa3be8ea79c328efb04d613433e9c8ad4c72a6ab
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-09 13:52:22 +1200

    Merge strings

--- src/C/strings.c
+++ src/C/strings.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
   char firstName[] = "Brendan";
   char lastName[] = {'J', 'a', 'r', 'v', 'i', 's', '\0'};
 
-  printf("Hello, %s %s!\n", firstName, lastName);
+  int firstNameLength = sizeof(firstName) / sizeof(firstName[0]);
+  int lastNameLength = sizeof(lastName) / sizeof(lastName[0]);
+
+  char fullName[firstNameLength + lastNameLength];
+
+  // copy firstName into fullName
+  for (int i = 0; i < firstNameLength; i++)
+  {
+    fullName[i] = firstName[i];
+  }
+
+  fullName[firstNameLength - 1] = ' ';
+
+  // copy lastName into fullName
+  for (int i = 0; i < lastNameLength; i++)
+  {
+    fullName[firstNameLength + i] = lastName[i];
+  }
+
+  system("clear");
+  printf("Full name: %s\n", fullName);
 }
