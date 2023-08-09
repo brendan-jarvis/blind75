6d24cf76ad6f4ebb7ee8010db72745db13e82871
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-09 12:16:58 +1200

    Multidimensional array

--- src/C/arrays.c
+++ src/C/arrays.c
@@ -6,8 +6,23 @@ int main()
   int coordinates[5] = {1, 2, 3, 4, 5};
   int coordinatesLength = sizeof(coordinates) / sizeof(coordinates[0]);
 
+  int multiDimensionalCoordinates[2][3] = {{1, 2, 3}, {4, 5, 6}};
+  int multiDimensionalCoordinatesLength = sizeof(multiDimensionalCoordinates) / sizeof(multiDimensionalCoordinates[0]);
+
+  system("clear");
+  printf("Array:\n");
   for (int i = 0; i < coordinatesLength; i++)
   {
     printf("%d\t", coordinates[i]);
+  }
+
+  printf("\nMultidimensional array:\n");
+  for (int i = 0; i < multiDimensionalCoordinatesLength; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      printf("%d\t", multiDimensionalCoordinates[i][j]);
+    }
+    printf("\n");
   }
 }
