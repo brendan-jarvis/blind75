265a8c9f63c74c7e8bcba29caac95c0746561fcb
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-09 14:17:14 +1200

    strcat

--- src/C/strings.c
+++ src/C/strings.c
@@ -1,30 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-  char firstName[] = "Brendan";
-  char lastName[] = {'J', 'a', 'r', 'v', 'i', 's', '\0'};
 
-  int firstNameLength = sizeof(firstName) / sizeof(firstName[0]);
-  int lastNameLength = sizeof(lastName) / sizeof(lastName[0]);
+  char s1[20] = "Brendan";
+  char s2[] = " Jarvis";
 
-  char fullName[firstNameLength + lastNameLength];
+  strcat(s1, s2);
 
-  // copy firstName into fullName
-  for (int i = 0; i < firstNameLength; i++)
-  {
-    fullName[i] = firstName[i];
-  }
-
-  fullName[firstNameLength - 1] = ' ';
-
-  // copy lastName into fullName
-  for (int i = 0; i < lastNameLength; i++)
-  {
-    fullName[firstNameLength + i] = lastName[i];
-  }
-
-  system("clear");
-  printf("Full name: %s\n", fullName);
+  printf("%s\n", s1);
 }
