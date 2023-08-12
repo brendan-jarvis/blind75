455bb39a6ab0a7175d27cdec32cde7ff8bb6ed0b
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-12 16:11:44 +1200

    Random number guessing game

--- src/C/loops.c
+++ src/C/loops.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+  srand(time(NULL));
+  int random = rand() % 20 + 1;
+  int guess;
+
+  printf("Guess the number between 1 and 20!\n");
+  scanf("%d", &guess);
+
+  while (guess != random)
+  {
+    if (guess > random)
+    {
+      printf("Too high! Guess again!\n");
+      scanf("%d", &guess);
+    }
+    else
+    {
+      printf("Too low! Guess again!\n");
+      scanf("%d", &guess);
+    }
+  }
+
+  printf("You guessed it! The number was %d!\n", random);
 }
