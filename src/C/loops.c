458c61d8e0c3da11cdd2cd72b1fe53adc850c9b7
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-15 19:37:45 +1200

    Add some hints

--- src/C/loops.c
+++ src/C/loops.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 int main()
 {
   srand(time(NULL));
   int random = rand() % 20 + 1;
   int guess;
+  int hints = 0;
 
   printf("Guess the number between 1 and 20!\n");
+
+  printf("Do you want some hints? (1 for yes, 0 for no)\n");
+
+  scanf("%d", &hints);
+
+  if (hints == 1)
+  {
+    printf("The number squared is:\t%d\n", (int)pow(random, 2));
+    printf("The number cubed is:\t%d\n", (int)pow(random, 3));
+  }
+  else if (hints == 0)
+  {
+    printf("Okay, no hints for you!\nEnter your guess:\n");
+  }
+
   scanf("%d", &guess);
 
   while (guess != random)
