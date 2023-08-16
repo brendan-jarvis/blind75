424274f1fd0cc9a3a3abedb1ca117c6cea58f885
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-16 16:35:41 +1200

    Person struct

--- src/C/structures.c
+++ src/C/structures.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+struct Person
+{
+  char firstName[20];
+  char lastName[20];
+  char country[30];
+};
+
+void printPerson(struct Person *person);
+
 int main(void)
 {
+  struct Person brendan = {"Brendan", "Jarvis", "New Zealand"};
+  struct Person jsBrendan = {"Brendan", "Eich", "United States of America"};
+
+  struct Person people[2] = {brendan, jsBrendan};
+
+  for (int i = 0; i < 2; i++)
+  {
+    printPerson(&people[i]);
+  }
+}
+
+void printPerson(struct Person *person)
+{
+  printf("\nFirst name:\t%s\n", person->firstName);
+  printf("Last name:\t%s\n", person->lastName);
+  printf("Country:\t%s\n", person->country);
 }
