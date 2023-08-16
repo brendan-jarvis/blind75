12a4851f4a6d095ccc585383299d8b4faf39cfd5
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-16 17:01:30 +1200

    Pig latin the names

--- src/C/structures.c
+++ src/C/structures.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdio.h>
 
 struct Person
 {
-  char firstName[20];
-  char lastName[20];
+  char firstName[26];
+  char lastName[26];
   char country[30];
 };
 
 void printPerson(struct Person *person);
+void printPeople(struct Person *people, int size);
+void pigLatinNames(struct Person *people, int size);
 
 int main(void)
 {
@@ -16,10 +20,10 @@ int main(void)
 
   struct Person people[2] = {brendan, jsBrendan};
 
-  for (int i = 0; i < 2; i++)
-  {
-    printPerson(&people[i]);
-  }
+  printPeople(people, 2);
+  printf("\nIn pig latin (after function call):\n");
+  pigLatinNames(people, 2);
+  printPeople(people, 2);
 }
 
 void printPerson(struct Person *person)
@@ -27,4 +31,45 @@ void printPerson(struct Person *person)
   printf("\nFirst name:\t%s\n", person->firstName);
   printf("Last name:\t%s\n", person->lastName);
   printf("Country:\t%s\n", person->country);
+}
+
+void printPeople(struct Person *people, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printPerson(&people[i]);
+  }
+}
+
+void pigLatinNames(struct Person *people, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    char pigLatinFirstName[29];
+    char pigLatinLastName[29];
+
+    // Convert first name to Pig Latin
+    if (strchr("aeiouAEIOU", people[i].firstName[0]) != NULL)
+    {
+      sprintf(pigLatinFirstName, "%sway", people[i].firstName);
+    }
+    else
+    {
+      sprintf(pigLatinFirstName, "%s%cay", people[i].firstName + 1, people[i].firstName[0]);
+    }
+
+    // Convert last name to Pig Latin
+    if (strchr("aeiouAEIOU", people[i].lastName[0]) != NULL)
+    {
+      sprintf(pigLatinLastName, "%sway", people[i].lastName);
+    }
+    else
+    {
+      sprintf(pigLatinLastName, "%s%cay", people[i].lastName + 1, people[i].lastName[0]);
+    }
+
+    // Update person's name
+    strcpy(people[i].firstName, pigLatinFirstName);
+    strcpy(people[i].lastName, pigLatinLastName);
+  }
 }
