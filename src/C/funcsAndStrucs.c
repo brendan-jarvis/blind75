0b6e84604442d60a373536a507ee9f30debe73a3
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-16 14:43:14 +1200

    func getRandomFloat

--- src/C/funcsAndStrucs.c
+++ src/C/funcsAndStrucs.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <time.h>
+
+float getRandomFloat(void)
+{
+  srand(time(NULL));
+  float randomDecimal = rand() % 100;
+  float random = randomDecimal / 100;
+
+  return random;
+}
 
 int main(void)
 {
-  int absValue = abs(-5);
-  printf("%d\n", absValue);
+  float random = getRandomFloat();
+
+  printf("Number: %f\n", random);
+  printf("Number ceiling: %d\n", (int)ceil(random));
+  printf("Number floor: %d\n", (int)floor(random));
+
   return 0;
 }
