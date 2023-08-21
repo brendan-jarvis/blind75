21eb19baef2ddb4b0bf5c748319e124613d90ce1
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-21 12:31:53 +1200

    Leap year checker

---
+++ src/C++/leap_year.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+  int year;
+
+  std::cout << "Enter the current year!\n";
+  std::cin >> year;
+
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+  {
+    std::cout << year << " is a leap year!\n";
+  }
+  else
+  {
+    std::cout << year << " is not a leap year!\n";
+  }
+}
