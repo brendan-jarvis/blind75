966ecb31834e110839039d33ccd9d97048d1e96b
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-21 13:51:13 +1200

    Struct vector

--- src/C++/vector.cpp
+++ src/C++/vector.cpp
@@ -4,6 +4,20 @@
 int main()
 {
 
+  struct City
+  {
+    std::string name;
+    double latitude;
+    double longitude;
+  };
+  std::vector<City> cities;
+
+  cities.push_back({"New York", 40.7128, -74.0060});
+  cities.push_back({"San Francisco", 37.7749, -122.4194});
+  cities.push_back({"Chicago", 41.8781, -87.6298});
+  cities.push_back({"Dallas", 32.7767, -96.7970});
+  cities.push_back({"Philadelphia", 39.9526, -75.1652});
+
   std::vector<double> cities_latitudes = {40.7128, 37.7749, 41.8781, 32.7767, 39.9526};
   std::vector<double> cities_longitudes = {-74.0060, -122.4194, -87.6298, -96.7970, -75.1652};
   std::vector<std::string> cities_names = {"New York", "San Francisco", "Chicago", "Dallas", "Philadelphia"};
@@ -17,5 +31,12 @@ int main()
   {
     std::cout << cities_names[i] << ": ";
     std::cout << cities_latitudes[i] << ", " << cities_longitudes[i] << "\n";
+  }
+
+  std::cout << "\nCities with struct:\n";
+  for (int i = 0; i < cities.size(); i++)
+  {
+    std::cout << cities[i].name << ": ";
+    std::cout << cities[i].latitude << ", " << cities[i].longitude << "\n";
   }
 }
