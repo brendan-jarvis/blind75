535de45f1cd45353ff1c9749790d4f388047ca64
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-21 15:01:10 +1200

    vector return type function

--- src/C++/functions.cpp
+++ src/C++/functions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 bool is_even(int number)
 {
@@ -20,18 +21,28 @@ int tenth_power(int num)
   return result;
 };
 
+std::vector<int> first_three_multiples(int num)
+{
+  std::vector<int> multiples;
+  for (int i = 1; i < 4; i++)
+  {
+    multiples.push_back(num * i);
+  }
+  return multiples;
+};
+
 int main()
 {
-  for (int i = 1; i < 20; i++)
+  std::cout << "is_even(8): \n";
+  is_even(8) ? std::cout << "true\n" : std::cout << "false\n";
+  std::cout << "\naverage(42.0, 24.0): \n";
+  std::cout << average(42.0, 24.0) << "\n";
+  std::cout << "\ntenth_power(3): \n";
+  std::cout << tenth_power(3) << "\n";
+  std::cout << "\nfirst_three_multiples(8): \n";
+  std::vector<int> multiples = first_three_multiples(8);
+  for (int i = 0; i < multiples.size(); i++)
   {
-    std::cout << i << " is ";
-    if (is_even(i))
-    {
-      std::cout << "even\n";
-    }
-    else
-    {
-      std::cout << "odd\n";
-    }
+    std::cout << multiples[i] << "\n";
   }
 }
