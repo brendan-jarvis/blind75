663854e89820810575e4d02541b52041c0290693
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-23 11:53:18 +1200

    Template function

--- src/C++/scope.cpp
+++ src/C++/scope.cpp
@@ -19,4 +19,13 @@ int main()
   std::cout << "Overload 5 (string)\n"
             << overload("5")
             << "!\n";
+
+  // Template functions
+  std::cout << "Template 5 (int)\n"
+            << template_function(5)
+            << "!\n";
+
+  std::cout << "Template 5 (double)\n"
+            << template_function(5.0)
+            << "!\n";
 }
