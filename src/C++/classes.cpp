93b53718f214eb8e411f5564d4e781b66fa33456
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-26 14:15:24 +1200

    Constructor

--- src/C++/classes.cpp
+++ src/C++/classes.cpp
@@ -3,21 +3,13 @@
 
 int main(){
   // create Song object
-  Song song;
-  song.setTitle("The Chain");
-  song.setArtist("Fleetwood Mac");
-  song.setAlbum("Rumours");
-  song.setGenre("Rock");
-  song.setYear(1977);
-
+  Song fleetwoodMacSong("Dreams", "Fleetwood Mac", "Rumours", "Rock", 1977);
 
   // print out song info
-  std::cout << "Title: " << song.getTitle() << "\n";
-  std::cout << "Artist: " << song.getArtist() << "\n";
-  std::cout << "Album: " << song.getAlbum() << "\n";
-  std::cout << "Genre: " << song.getGenre() << "\n";
-  std::cout << "Year: " << song.getYear() << "\n";
-
-  return 0;
-
+  // std::cout << "Title: " << fleetwoodMacSong.getTitle() << "\n";
+  // std::cout << "Artist: " << fleetwoodMacSong.getArtist() << "\n";
+  // std::cout << "Album: " << fleetwoodMacSong.getAlbum() << "\n";
+  // std::cout << "Genre: " << fleetwoodMacSong.getGenre() << "\n";
+  // std::cout << "Year: " << fleetwoodMacSong.getYear() << "\n";
+  std::cout << fleetwoodMacSong.getSongInfo() << "\n";
 }
