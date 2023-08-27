de955cd1f00bbaad75cd5f17650a6314e8164f31
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-27 20:57:33 +1200

    Alt initialisation

--- src/C++/classes.cpp
+++ src/C++/classes.cpp
@@ -4,24 +4,24 @@
 
 int main(){
   int count = 0;
-  std::vector<Song> songs[5];
-
-  songs->push_back(Song("The End", "The Doors", "The Doors", "Rock", 1967));
-  songs->push_back(Song("Dreams", "Fleetwood Mac", "Rumours", "Rock", 1977));
-  songs->push_back(Song("Stairway to Heaven", "Led Zeppelin", "Led Zeppelin IV", "Rock", 1971));
-  songs->push_back(Song("Bohemian Rhapsody", "Queen", "A Night at the Opera", "Rock", 1975));
-  songs->push_back(Song("Hotel California", "Eagles", "Hotel California", "Rock", 1976));
+  std::vector<Song> songs = {
+    Song("The End", "The Doors", "The Doors", "Rock", 1967),
+    Song("Dreams", "Fleetwood Mac", "Rumours", "Rock", 1977),
+    Song("Stairway to Heaven", "Led Zeppelin", "Led Zeppelin IV", "Rock", 1971),
+    Song("Bohemian Rhapsody", "Queen", "A Night at the Opera", "Rock", 1975),
+    Song("Hotel California", "Eagles", "Hotel California", "Rock", 1976)
+  };
   
-  if (songs->size() == 1)
-    std::cout << "There is " << songs->size() << " song:\n";
-  else if (songs->size() > 1)
+  if (songs.size() == 1)
+    std::cout << "There is " << songs.size() << " song:\n";
+  else if (songs.size() > 1)
   {
-    std::cout << "There are " << songs->size() << " songs:\n";
+    std::cout << "There are " << songs.size() << " songs:\n";
   }
 
-  for (int i = 0; i < songs->size(); i++)
+  for (int i = 0; i < songs.size(); i++)
   {
     std::cout << "\nSong #" << i + 1 << "\n";
-    std::cout << songs->at(i).getSongInfo() << "\n";
+    std::cout << songs.at(i).getSongInfo() << "\n";
   }
 }
