69759d11a862ea233a47dd8a141aa0add40a73aa
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-08-29 15:16:38 +1200

    nullptr assignment

--- src/C++/pointers.cpp
+++ src/C++/pointers.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "pointers_functions.h"
 
-int main(){
+int main()
+{
   int a = 5;
-  int* b = &a;
+  int *b = &a;
+  int *c = nullptr;
 
   std::cout << welcomeMessage();
   std::cout << "Variable a = " << a << " and variable b = " << *b << "\n";
@@ -17,4 +19,11 @@ int main(){
 
   std::cout << "Memory addresses:\n";
   std::cout << "a = " << &a << "\tb = " << &b << "\n";
+
+  std::cout << "c = " << c << "\n";
+  std::cout << "nullptr allows for the creation of a pointer that does not point to anything.\n"
+            << "This is useful for initializing pointers that will be set later in the program.\n";
+  std::cout << "Assigning c = &a\n";
+  c = &a;
+  std::cout << "c = " << c << " = " << *c << "\n";
 }
